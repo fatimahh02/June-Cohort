da447a5745333b510978f22a2de8442d44af6a25
fatimahh02 <isakjifatimah02@gmail.com>
2023-07-22 14:11:36 +0100

    lesson3.js

---
+++ javascript/lesson3.js
@@ -0,0 +1,16 @@
+//conditional statements
+
+if(condition){
+    dothis 
+}else{
+    dothis
+}
+
+//logic gates
+//ANDgate (&&)
+//ORgate(||)
+
+//= setting variable
+//==loose translation
+//=== strict 
+
