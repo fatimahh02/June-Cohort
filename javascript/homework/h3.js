ceced83cd8845ec39bcbb650212115f27d084dc9
fatimahh02 <isakjifatimah02@gmail.com>
2023-07-27 16:31:42 +0100

    h3.js

---
+++ javascript/homework/h3.js
@@ -0,0 +1,36 @@
+//Write a conditional statement to state if hero is a dc character or marvel character. (i.e superman would be dc, spiderman would be marvel).
+
+ var hero ="superman"
+
+ if(hero==="superman"){
+ console.log("DC")
+ }else if(hero==="spiderman"){
+     console.log("Marvel")
+}else{
+     console.log("error")
+ }
+
+//write a conditional statement taking age as variable and what year group they will be in a school 
+
+ let age=17
+ if(age>=11 && age <16){
+     console.log("secondary education")
+ }else if (age>=16 && age<18){
+     console.log("college")
+ }else if (age>=18){
+     console.log("optional education")
+ }else{
+     ("error")
+ }
+
+
+//Create a shopping list of 5 items. Now make a conditional statement that checks if the list contains 5 items only. if above 5 print out error and if less than 5 print out error. 
+
+// let array =["bread","milk","eggs","butter","jam"]
+//  if(array.length>5 && array.length<5){
+//     console.log(array,"ERROR")
+//  }else{
+//     ("array is 5")
+//  }
+
+
