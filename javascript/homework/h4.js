bb8f306c3be1ff013a452cbc92b5774a0e0ae3fe
fatimahh02 <isakjifatimah02@gmail.com>
2023-08-01 20:57:10 +0100

    l5.js

--- javascript/homework/h4.js
+++ javascript/homework/h4.js
@@ -4,7 +4,7 @@ let month="JULY".toLowerCase()
 
 
 if(month="july"){
-console.log(`${month} is a month`)
+console.log("that is a month")
 }else{
     console.log(errror)
 }
@@ -13,6 +13,7 @@ console.log(`${month} is a month`)
 
 let phone="iphone"
 
+if(typeof phone==="string")
 if(phone="iphone" || phone=="iphone3g" || phone=="iphone3gs" || phone=="iphone4" || phone=="iphone4s"|| phone=="iphone5"|| phone=="iphone5c"|| phone=="iphone5s"|| phone=="iphone6"|| phone=="iphone6plus"|| phone=="iphone6s"|| phone=="iphone6splus"|| phone=="iPhone SE"|| phone=="iPhone 7"|| phone=="iPhone 7 Plus"|| phone=="iPhone 8"|| phone=="iphone8 plus"|| phone=="iPhone X"|| phone=="iPhone XR"|| phone=="iPhone XS"|| phone=="iPhone 11" || phone=="iPhone 11 Pro" || phone=="iPhone 11 Pro Max" || phone=="iPhone 12 mini"|| phone=="iPhone 12" || phone== "iPhone 12 Pro" || phone=="iPhone 12 Pro Max"|| phone=="iPhone 13 mini"|| phone=="iPhone 13"|| phone=="Phone 13 Pro"|| phone=="iPhone 13 Pro Max"|| phone=="iPhone 14"|| phone=="iPhone 14 Plus"|| phone=="iPhone 14 Pro"|| phone=="iPhone 14 Pro Max"){
     console.log(`${phone} is an iphone`)
 }else if(phone=="android"){
@@ -23,10 +24,11 @@ if(phone="iphone" || phone=="iphone3g" || phone=="iphone3gs" || phone=="iphone4"
 
 //Write a switch case that checks if the operation is a math operation (+,-,*,/) 
 
-var operation="="
+var operation="*"
 var no1 =250
 var  no2= 10
 
+if(typeof operation==="string")
 switch(operation){
     case"+":
     console.log(no1+ no2)
---
+++ javascript/lesson5.js
@@ -0,0 +1,9 @@
+//create a loop checks if numbers are odd or even
+
+
+for(let i=0; i<=100; i++){
+    if(i%2===0){
+    console.log(i+"even number")
+}else{
+    console.log(i+"odd number")}
+}
