febb195d3f5697b310562aa3c4531c4161b60c91
fatimahh02 <isakjifatimah02@gmail.com>
2023-08-08 20:55:26 +0100

    lesson6.js

--- javascript/lesson5.js
+++ javascript/lesson5.js
@@ -1,9 +1,12 @@
 //create a loop checks if numbers are odd or even
 
 
-for(let i=0; i<=100; i++){
-    if(i%2===0){
-    console.log(i+"even number")
-}else{
-    console.log(i+"odd number")}
+for(let i=1; i<=100; i=i+1){
+    console.log(i)
+    if(i%3==3){
+    console.log(i+"fizz")
+    }
 }
+// }else{
+//     console.log(i+"odd number")}
+// }
---
+++ javascript/lesson6.js
@@ -0,0 +1,32 @@
+// functions are private enterties unless they are declared seperately they will not run
+// function printAge(){
+
+// const age = 35
+// console.log(age)
+
+// }
+
+// printAge()
+// const age =25
+// console.log(age)
+
+
+
+//parameters
+
+// function printAge(age){
+// console.log(age)
+// }
+    
+// printAge(25)
+
+   
+function toCelcius(farenheit){
+    return(farenheit-32)*5/9
+}
+    
+function toFarenheit(celsius){
+    return(celsius*9/5)+32
+}
+    
+ 
