8014a8221a3b4d55975a1ad65bcf931461f12185
fatimahh02 <isakjifatimah02@gmail.com>
2023-08-11 23:58:38 +0100

    h6

---
+++ javascript/homework/h6.js
@@ -0,0 +1,16 @@
+//*Given a word check to see if the reverse of the word is correct (i.e is the word a palindrome). Example words;
+
+
+
+//create a function that converts farenheit to kelvin
+function farenheit(num){
+    return num
+}
+
+function farenheittokelvin(num){
+    var converter= farenheit(num)
+    var kelvin =(converter- 32) * 5 / 9 + 273.15
+    console.log(kelvin)
+}
+
+farenheittokelvin(1)
